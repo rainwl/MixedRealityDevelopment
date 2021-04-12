2bb622cb7581ce3dbaa317d55eecc0de84200d3f
rainwl <2215338912@qq.com>
2021-04-12 23:48:50 +0800

    代码
    1.复制粘贴对象拥有的所有组件
    2.播放animation动画

---
+++ PlayAni.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayAni : MonoBehaviour
+{
+    public AnimationClip Playanim;
+    public AnimationClip Endanim;
+  
+    public void Playani()
+    {
+        Animation animation = GetComponent<Animation>();
+        animation.Play(Playanim.name);
+    }
+    public void Endani()
+    {
+        Animation animation = GetComponent<Animation>();
+        animation.Play(Endanim.name);
+    }
+}
